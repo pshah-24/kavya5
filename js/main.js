2b3216a39252cfe44687799d7e4e87e3775d26e4
pshah24 <77745358+pshah24-robotics@users.noreply.github.com>
2025-02-07 05:48:24 -0500

    d

--- js/main.js
+++ js/main.js
@@ -13,4 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
             noButton.style.left = randomX + "px";
             noButton.style.top = randomY + "px";
         });
-    });
+   });
+
+let myAudio = new Audio("img/Floricienta.mp3"); // Create an Audio object with the file 'Ceylon.mp3'
+
+window.onload = function() {
+    setInterval(GameLoop, 1000 / 10); // Start the game loop when the window is loaded
+};
+
+function GameLoop() {
+    if (myAudio.paused) { // Check if the audio is paused
+        myAudio.play(); // If paused, play the audio
+    }
+}
